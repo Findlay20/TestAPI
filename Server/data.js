23d50a8f92df5f23ce4bb038c4d68a48696134b8
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-10-17 13:16:14 +0100

    Added data.js and filled with data

---
+++ Server/data.js
@@ -0,0 +1,8 @@
+const movies = [
+    {id: 1, title: "Minions", "release-date": "2015", rating: 6.4},
+    {id: 2, title: "Whiplash", "release-date": "2014", rating: 8.5},
+    {id: 3, title: "Minions", "release-date": "2015", rating: 6.4},
+]
+
+
+module.exports = movies;
