0f24f0980181a3838a6983e675958edc17bf99a2
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-10-17 13:57:07 +0100

    Added functinoality to server.js - Can now see all movies at url/movies

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const movies = require('../Server/data')
 
 const app = express();
 
 app.use(express.json())
 app.use(cors());
+
+//See all movies
+app.get('/movies', (req, res) => {
+    res.send(movies);
+});
 
 
 
