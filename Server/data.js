4c8fa35697f6314825c774c4eb9a3d44fc255932
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-10-17 14:28:25 +0100

    Added 'find by id' and 'add movie' functionality

--- Server/data.js
+++ Server/data.js
@@ -1,7 +1,7 @@
 const movies = [
-    {id: 1, title: "Minions", "release-date": "2015", rating: 6.4},
-    {id: 2, title: "Whiplash", "release-date": "2014", rating: 8.5},
-    {id: 3, title: "Minions", "release-date": "2015", rating: 6.4},
+    {id: 1, title: "Minions", releaseDate: "2015", rating: 6.4},
+    {id: 2, title: "Whiplash", releaseDate: "2014", rating: 8.5},
+    {id: 3, title: "Minions", releaseDate: "2015", rating: 6.4},
 ]
 
 
